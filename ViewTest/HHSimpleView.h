2306809c17988b47bf0d7261e5abd059d45a5983
Hugo <hbh02012@pomona.edu>
2014-09-10 19:36:46 -0700

    viewTest tutorial

---
+++ ViewTest/HHSimpleView.h
@@ -0,0 +1,13 @@
+//
+//  HHSimpleView.h
+//  ViewTest
+//
+//  Created by Hugo Ho on 9/10/14.
+//  Copyright (c) 2014 Hugo Ho. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HHSimpleView : UIView
+
+@end
---
+++ ViewTest/HHSimpleView.m
@@ -0,0 +1,13 @@
+//
+//  HHSimpleView.m
+//  ViewTest
+//
+//  Created by Hugo Ho on 9/10/14.
+//  Copyright (c) 2014 Hugo Ho. All rights reserved.
+//
+
+#import "HHSimpleView.h"
+
+@implementation HHSimpleView
+
+@end
